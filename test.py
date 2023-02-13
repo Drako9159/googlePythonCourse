485849d01f4d68462c0c939195b6ba53535d7818
drako9159 <drako9159@gmail.com>
2023-02-13 17:33:24 -0600

    add challenge complete

--- test.py
+++ test.py
@@ -1,11 +1,27 @@
+#!/usr/bin/env python3
+import sys
 import os
-
-def create_python_script(filename):
-  comments = "# Start osdflkñlsdfjlñgjsdfñlgjñlakjdfñgljsdfgf a new Python program"
-  with open(filename, "w") as file:
-    file.write(comments)
+import re
+def error_search(log_file):
+  error = input("What is the error? ")
+  returned_errors = []
+  with open(log_file, mode='r',encoding='UTF-8') as file:
+    for log in  file.readlines():
+      error_patterns = ["error"]
+      for i in range(len(error.split(' '))):
+        error_patterns.append(r"{}".format(error.split(' ')[i].lower()))
+      if all(re.search(error_pattern, log.lower()) for error_pattern in error_patterns):
+        returned_errors.append(log)
     file.close()
-    filesize = os.path.getsize(filename)
-  return(filesize)
-
-print(create_python_script("program.py"))
+  return returned_errors
+  
+def file_output(returned_errors):
+  with open(os.path.expanduser('~') + '/data/errors_found.log', 'w') as file:
+    for error in returned_errors:
+      file.write(error)
+    file.close()
+if __name__ == "__main__":
+  log_file = sys.argv[1]
+  returned_errors = error_search(log_file)
+  file_output(returned_errors)
+  sys.exit(0)
